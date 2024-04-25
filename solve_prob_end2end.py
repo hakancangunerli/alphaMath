1c0bed97eed00340dac7b0da3c7592395a94968b
Wei Guo <weiguo200101@gmail.com>
2024-04-25 13:35:04 -0400

    update solver for rag and code hints. create solve problem end to end

---
+++ solve_prob_end2end.py
@@ -0,0 +1,76 @@
+from judge_correctness import judge_correctness
+from solver import solve_problem_by_coding, solve_problem
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
+rag_data_base = Chroma(persist_directory="./chroma_db", 
+             embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"))
+
+def solve_prob_end2end(problem:str,
+                data_class: str,
+               correct_solution:str,
+    coding_llm:str,
+    main_solver_llm:str,
+    judging_llm:str,
+    coding_max_attempt:int=5,
+    test_mode:bool=False,):
+    """
+    The end to end process to solve a problem.
+
+    Output:
+        True if the problem has been solved correctly, 
+        False if the problem has not been solved correctly,
+        ValueError if the judging LLM's response is invalid
+    """
+    
+    # query the rag system
+    rag_items=dict()
+    for doc in rag_data_base.similarity_search(problem):
+        rag_items[doc.page_content]=doc.metadata["solution"]
+    if not rag_items:
+        rag_result=None
+    else:
+        rag_result=[]
+        for idx,(k,v) in enumerate(rag_items.items(),1):
+            rag_result.append(f'Example problem {idx}: {k}\n{"Solution: "+v}')
+        rag_result='\n'.join(rag_result)
+
+    if test_mode:
+        print('Information retrieved from RAG: ```{rag_result}```')
+
+    # solve the problem by coding
+    coding_res=solve_problem_by_coding(problem,
+                                       data_class,
+                                       coding_llm,
+                                       max_attempt=coding_max_attempt,
+                                       rag_hint=rag_result,
+                                       test_mode=test_mode)
+    if test_mode:
+        print('The result from coding:', coding_res)
+
+    # solve the problem by the main LLM
+    main_solver_res=solve_problem(problem,
+                                  data_class,
+                                  main_solver_llm,
+                                  rag_hint=rag_result,
+                                  coding_hint=coding_res,
+                                  test_mode=test_mode)
+    if test_mode:
+        print('The result from the main solver:', main_solver_res)
+
+    # validate the correctness of the results
+    return judge_correctness(problem,
+                                  correct_solution,
+                                  main_solver_res,
+                                  llm=judging_llm)
+
+
+if __name__=='__main__':
+    print(solve_prob_end2end(
+        problem='What is the integral of $f(x)=e^{-x^2/2}$ on R?',
+               data_class='pre-calculus',
+                correct_solution='$\sqrt{2\pi}$',
+                coding_llm='llama3-70b-8192',
+                main_solver_llm='llama3-70b-8192',
+                judging_llm='llama3-70b-8192',
+                test_mode=True))
--- solver.py
+++ solver.py
@@ -45,31 +45,36 @@ def exec_capture_stdout(code):
 
 
 def solve_problem(
-    problem: str, data_class: str, solver_llm: str, hint: str = None, test_mode=False
+    problem: str, data_class: str, solver_llm: str, 
+    rag_hint: str = None,
+    coding_hint:str=None,
+    test_mode=False
 ):
     """
     parameters:
     - problem: the problem to solve
     - data_class: the class of the problem, must be in ["algebra","counting_and_probability", "geometry", "intermediate_algebra", "number_theory", "prealgebra", "precalculus"]
     - solver_llm: the llm used to solve the problem
-    - hint: the hint (obtained from RAG / code / ...) to solve the problem, default None
+    - rag_hint: the hint (obtained from RAG) to solve the problem, default None
+    - coding_hint: the hint (obtained from coding LLM) to solve the problem, default None
 
     TODO: add other parameters (e.g., temperature)
 
     return:
-        the solution to the problem
+        the solution to the problem, a string
     """
 
-    instruct_query = f"""Imagine you are a math expert in {data_class}, who is solving a math problem in all seriousness during the exam. You will be given a math problem in LaTeX format wrapped in three backticks (```), which is well-posed and has a unique solution. Your task is to provide a complete, detailed and clear solution to it in LaTeX form, as well as rendering all the calculation steps. Your last sentense MUST be 'Therefore, the solution is XXX', which outputs the final solution. Be extremely accurate. No mistakes allowed."""
+    instruct_query = f"""Imagine you are a math expert in {data_class}, who is solving a math problem in all seriousness during the exam. You will be given a math problem in LaTeX format wrapped in three backticks (```), which is well-posed and has a unique solution. We might also give you some hints, if available. Your task is to use all everything we give you to provide a complete, detailed and clear solution to the math problem we give you, in LaTeX form, as well as rendering all the calculation steps. Your last sentense MUST be 'Therefore, the solution is XXX', which outputs the final solution. Be extremely accurate. No mistakes allowed."""
 
     user_query = (
-        f"""Here is the problem you need to solve:\n\n```\n{problem}\n```\n\n"""
+        f"""Here is the problem you need to solve:\n```\n{problem}\n```\n"""
     )
 
-    if hint is not None:
-        user_query += (
-            f"""Let me give you some possibly useful hints:\n```{hint}```\n\n"""
-        )
+    if rag_hint is not None:
+        user_query+=f"""Here are some example problems and their solutions to help you better solve the problem I give you:\n```{rag_hint}```\n\n"""
+
+    if coding_hint is not None:
+        user_query+=f"""We have also written some Python script to solve that problem. The result from running these codes is `{coding_hint}`. This may not be 100% correct, but may be useful for you to validate the answer. Please use with care.\n"""
 
     if test_mode:
         print(f"query sent to {solver_llm}:\n", instruct_query + user_query)
@@ -84,6 +89,7 @@ def solve_problem_by_coding(
     data_class: str,
     solver_llm: str,
     max_attempt: int = 5,
+    rag_hint: str = None,
     test_mode=False,
 ):
     """
@@ -92,16 +98,20 @@ def solve_problem_by_coding(
     - data_class: the class of the problem
     - coding_llm: the language model to use
     - max_attempt: the maximum number of attempts to execute the code if it fails
+    - rag_hint: the hint (obtained from RAG) to solve the problem, default None
 
     return:
         the result of the code execution, if it succeeds in max_attempt attempts; otherwise None
     """
 
-    instruct_query = f"Imagine that you are an expert programmer that writes simple, concise code in Python. You will be given a mathematical problem in the domain of {data_class}, and your goal is to write some simple and executable python code to solve the problem. ONLY the code is needed, Absolutely NO explanation. In case you still want to add explanation, please all explanations must be done in the form of COMMENTS in python. You are recommended to use SymPy for symbolic computation."
+    instruct_query = f"Imagine that you are an expert programmer that writes simple, concise code in Python. You will be given a mathematical problem in the domain of {data_class}, and your goal is to write some simple and executable python code to solve the problem. ONLY the code is needed, absolutely NO explanation. In case you still want to add explanation, please all explanations must be done in the form of COMMENTS in python. You are recommended to use SymPy for symbolic computation."
 
     user_query = (
         f"""Here is the problem you need to solve:\n\n```\n{problem}\n```\n\n"""
     )
+    if rag_hint is not None:
+        user_query+=f"""Here are some example problems and their solutions to help you better solve the problem I give you:\n```{rag_hint}```\n\n"""
+
     response = call_llm_api(
         model=solver_llm, system_query=instruct_query, user_query=user_query
     )
